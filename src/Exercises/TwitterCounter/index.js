bc2f3250f993f90f21c825c608c97ad7ae64b4a5
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-02-20 08:42:18 +0530

    Bugs Fixed

--- src/Exercises/TwitterCounter/index.js
+++ src/Exercises/TwitterCounter/index.js
@@ -5,16 +5,13 @@ import './styles.css';
 
 
 
-const TwitterCounter = () => {
+const Function = () => {
 	const [count, setCount] = useState(0);
 	const remainingChar = (e) => {
 		setCount(40 - e.target.value.length);
 	}
 	return (
 		<>
-			<p className='question'>Ex1: character counter
-				See the Twitter post component. It gives you an indicator of the character count. Replicate that behavior.</p>
-			<h1>Ex1:Twitter character counter</h1>
 			<textarea
 				cols="40"
 				rows="5"
@@ -30,9 +27,15 @@ const TwitterCounter = () => {
 
 
 
-
-
-
-
-
-export default TwitterCounter
+export default function Exercise() {
+	return (
+		<>
+			<h1>Ex1:Twitter character counter</h1>
+			<p className='question display-block center'>Ex1: character counter
+				See the Twitter post component. It gives you an indicator of the character count. Replicate that behavior.</p>
+			<br />
+			<br />
+			<Function />
+		</>
+	)
+};
